00ae6746415a6f4416b257bb601852e1e651188c
kwek <jochem.leijenhorst@hva.nl>
2024-01-02 14:29:19 +0100

    Finish players and point-storage

--- generate.php
+++ generate.php
@@ -14,19 +14,20 @@ if(isset($_POST["name"])) {
         die;
     }
     setcookie("name", $_POST["name"]);
-    getPlayerData($_POST["name"]);
+    $name = $_POST["name"];
 }
-
 // Ask for a name.
-if(!isset($_COOKIE["name"]) && !isset($_POST["name"])) {
+else if(!isset($_COOKIE["name"])) {
     printNameRequest();
     die;
 }
+else $name = $_COOKIE["name"];
 
 // Get user info.
-if(isset($_COOKIE["name"])) $name = $_COOKIE["name"];
-else $name = $_POST["name"];
 $player = getPlayerData($name);
+echo "<pre>";
+var_dump($player);
+echo "</pre>";
 
 // Check provided answer. This has to happen here to update the user info.
 if(isset($_POST["question"]) && isset($_POST["answer"])) {
@@ -35,7 +36,7 @@ if(isset($_POST["question"]) && isset($_POST["answer"])) {
 
 // Generate user info thingy.
 echo "<div>";
-echo "Hello <b>$name</b>.<br>You currently have <b>$player->points</b> points, from <b>$player->answers</b> correct answers.";
+echo "Hello <b>$name</b>.<br>You currently have <b>$player->points</b> points, from <b>$player->answerCount</b> correct answers.";
 echo "</div><br><br>";
 
 // Parse question file.
@@ -45,14 +46,14 @@ fclose($qFile);
 
 // Generate questions.
 foreach($questions as $q) {
-    echo "<div id=\"$q->id\">";
-    generateAnswers($q);
+    echo "<div id='$q->id'>";
+    generateAnswers($player, $q);
     echo "<div>$q->html</div>";
-    echo "<form method=\"post\" action=\"#$q->id\">";
-    echo "<label for=\"$q->id-answer\">Answer: </label>";
-    echo "<input type=\"text\" name=\"answer\" id=\"$q->id-answer\">";
-    echo "<input type=\"hidden\" name=\"question\" value=\"$q->id\">";
-    echo "<br><input type=\"submit\">";
+    echo "<form method='post' action='#$q->id'>";
+    echo "<label for='$q->id-answer'>Answer: </label>";
+    echo "<input type='text' name='answer' id='$q->id-answer'>";
+    echo "<input type='hidden' name='question' value='$q->id'>";
+    echo "<br><input type='submit'>";
     if(isset($checkedAnswer) && $_POST["question"] == $q->id) {
         echo "<br><br><b>You answered:</b> " . $_POST["answer"] . "<br>";
         echo "<br>$checkedAnswer";
@@ -62,23 +63,36 @@ foreach($questions as $q) {
 
 fclose($player->file);
 
-function generateAnswers($question) {
-    $questionCount = count($question->points);
-    echo "<div class=\"points\">";
+function generateAnswers($player, $question) {
+    // Count the amount of point values the question has in questions.json.
+    $answerCount = count($question->points);
+    echo "<div class='points'>";
     
     // Singular answer questions:
-    if($questionCount == 1) {
-        echo "<b>Answer: </b><div>" . $question->points[0] . " point" .
-            ($question->points[0] > 1 ? "s" : "") . 
-        ".</div>";
+    if($answerCount == 1) {
+        echo "<b>Answer: </b><div";
+        // Check if the question has been answered already.
+        if(isset($player->answers[$question->id]) && $player->answers[$question->id][0] == 0)
+            echo " class='answered'";
+
+        echo ">" . $question->points[0] . " point";
+
+        // Pluralize 'point' if necessary.
+        if($question->points[0] > 1) echo "s"; 
+
+        echo ".</div>";
     }
     // Multi answer questions:
     else {
         echo "<b>Answers: </b>";
-        for($i = 0; $i < $questionCount; $i++) {
-            echo "<div>" . ($i + 1) . ": " . $question->points[$i] . " point" . 
-                ($question->points[$i] > 1 ? "s" : "") . // Gotta have that responsive pluralization
-            ".</div>";
+        for($i = 0; $i < $answerCount; $i++) {
+            echo "<div";
+            // Check if the question has been answered already.
+            if(isset($player->answers[$question->id]) && in_array($i, $player->answers[$question->id]))
+                echo " class='answered'";
+            echo ">" . ($i + 1) . ": " . $question->points[$i] . " point";
+            if($question->points[$i] > 1) echo "s"; // Gotta have that responsive pluralization
+            echo ".</div>";
         }
     }
     echo "</div>";
@@ -95,11 +109,17 @@ function checkAnswer($player, $questionId, $userAnswer) {
             foreach($pair->patterns as $pattern) {
                 // Check answer.
                 if(stristr($userAnswer, $pattern)) {
+                    $ret = "";
+                    // Check if it's correct or already answered.
+                    if(
+                        $answer->correct == true 
+                        && addAnswerToPlayer($player, $questionId, $answerIndex, $answer->points)
+                    ) $ret .= "(<i>You already answered this question with this answer.</i>)<br><br>";
+                        
                     // If it matches and it's a correct answer, store it.
-                    if($answer->correct == true) {
-                        addAnswerToPlayer($player, $questionId, $answerIndex, $answer->points);
-                    }
-                    return $pair->response;
+                    $ret .= $pair->response;
+                        
+                    return $ret;
                 }
             }
         }
@@ -109,9 +129,9 @@ function checkAnswer($player, $questionId, $userAnswer) {
 }
 
 function printNameRequest() {
-    echo "<div><form method=\"post\">";
-    echo "<label for=\"name\">Please provide a name :)</label><br>";
-    echo "<input type=\"text\" name=\"name\" id=\"name\">";
+    echo "<div><form method='post'>";
+    echo "<label for='name'>Please provide a name :)</label><br>";
+    echo "<input type='text' name='name' id='name'>";
     echo "</form></div>";
 }
 
--- players.php
+++ players.php
@@ -8,26 +8,47 @@ function getPlayerData($name) {
     // 0    1          2           3
 
     $points = 0;
-    $answers = 0;
+    $answerCount = 0;
+    $answers = [];
     $line = fgetcsv($file);
+
     while($line != false) {
-        $answers++;
+        $answerCount++;
         $points += intval($line[3]);
+
+        // Add the answered answers to an array indexed by question ID.
+        // If the array is this: ["question1" => [0,2], "question2" => [0]],
+        //   it means that question1 has been answered with answers 0 and 2,
+        //   and question2 has been answered with answer 0.
+        if(!isset($answers[$line[1]]))
+            $answers[$line[1]] = [$line[2]];
+        else
+            array_push($answers[$line[1]], $line[2]);
 
         $line = fgetcsv($file);
     }
 
-    return (object) array('file' => $file, 'answers' => $answers, 'points' => $points);
+    return (object) array('file' => $file, 'answerCount' => $answerCount, 'answers' => $answers, 'points' => $points);
 }
 
 function addAnswerToPlayer($player, $questionId, $answerIndex, $points) {
-    $fields = [time(), $questionId, $answerIndex, $points];
+    
+    // Check if answer has been answered before.
+    if(isset($player->answers[$questionId])) {
+        if(in_array($answerIndex, $player->answers[$questionId]))
+        return true;
+    else array_push($player->answers[$questionId], $answerIndex);
+    }
+    else $player->answers[$questionId] = [$answerIndex];
 
+    $fields = [time(), $questionId, $answerIndex, $points];
     fputcsv($player->file, $fields);
 
     // Update data in current memory.
     $player->points += $points;
-    $player->answers++;
+    $player->answerCount++;
+
+    return false;
 }
 
 
