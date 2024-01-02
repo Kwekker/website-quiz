dba689a86186c51b839b8f98a563897e240c315d
kwek <jochem.leijenhorst@hva.nl>
2024-01-02 01:47:50 +0100

    Improve the styling of the site

--- generate.php
+++ generate.php
@@ -32,7 +32,7 @@ $cAnswers = 69;
 
 echo "<div>";
 echo "Hello <b>$name</b>.<br>You currently have <b>$points</b> points, from <b>$cAnswers</b> correct answers.";
-echo "</div>";
+echo "</div><br><br>";
 
 
 // Generate questions.
@@ -45,8 +45,9 @@ foreach($questions as $q) {
     echo "<label for=\"$id-answer\">Answer: </label>";
     echo "<input type=\"text\" name=\"answer\" id=\"$id-answer\">";
     echo "<input type=\"hidden\" name=\"question\" value=\"$id\">";
-    echo "<br><br><input type=\"submit\">";
+    echo "<br><input type=\"submit\">";
     if($answerQ == $id) {
+        echo "<br><br><b>You answered:</b> $answer<br>";
         echo "<br>" . checkAnswer($answerQ, $answer);
     }
     echo "</form></div>";
@@ -54,16 +55,21 @@ foreach($questions as $q) {
 
 function generateAnswers($question) {
     $questionCount = count($question["points"]);
-    echo "<div class=\"points\"> <b>Answer:</b>";
+    echo "<div class=\"points\">";
     
     // Singular answer questions:
     if($questionCount == 1) {
-        echo "<div>" . $question["points"][0] . " points. </div>";
+        echo "<b>Answer: </b><div>" . $question["points"][0] . " point" .
+            ($question["points"][0] > 1 ? "s" : "") . 
+        ".</div>";
     }
     // Multi answer questions:
-    else { 
+    else {
+        echo "<b>Answers: </b>";
         for($i = 0; $i < $questionCount; $i++) {
-            echo "<div>" . ($i + 1) . ": " . $question["points"][$i] . " points</div>";
+            echo "<div>" . ($i + 1) . ": " . $question["points"][$i] . " point" . 
+                ($question["points"][$i] > 1 ? "s" : "") . // Gotta have that responsive pluralization
+            ".</div>";
         }
     }
     echo "</div>";
--- index.php
+++ index.php
@@ -28,6 +28,8 @@
     <h2 style="width:90%;margin-left: 5%;"> 
       A quiz with really obscure questions
     </h2>
+
+
     
   </article>
 
@@ -43,20 +45,23 @@
 
   <article>
     <div class="unuglify">
+      <portal id="exampleportal" src="https://jochemleijenhorst.com/" height="300px"></portal>
+
       <h3>How it works</h3>
       Scroll through the list of dumb questions. If you know the answer to any of the questions, put the answer into the accompanying answer field.<br>
       Not every 'question' is actually a question. Some are just quotes or images that reference something. In these cases, just answer with what you think it is referencing.<br>
       A few details about answering:
       <ul>
         <li>Answers are case-insensitive.</li>
-        <li>Most of the time the program only checks if your answer <i>includes</i> the correct answer. If the answer to a question is "blue" and you answer with "it's blue", it'll get accepted.</li>
-        <li>Most questions have multiple correct answers. For example, the answer to "what color is the sky" could be both "blue" and "black", because it's not always day time. Sometimes multiple answers even give you an amount of points individually.</li>
+        <br><li>Most of the time the program only checks if your answer <i>includes</i> the correct answer. If the answer to a question is "blue" and you answer with "it's blue", it'll get accepted.</li>
+        <br><li>Every correct answer gives you an amount of points. The amount of points is based on how cool I think it is that you can answer that specific question with that specific answer.</li>
+        <br><li>Some questions have multiple correct answers. For example, the answer to "What color is the sky?" could be both "blue" and "black", because it's not always day time. Sometimes these answers even give you an amount of points individually.</li>
       </ul>
-      Every correct answer gives you an amount of points. The amount of points is based on how cool I think it is that you can answer that specific question with that specific answer.
-      <br><br>
+      
+      
       I have tried to keep the questions as ungoogleable as possible, but still please just don't try; it ruins the whole purpose of this page.
       <br><br>
-      TL;DR Just answer the fuckin questions.
+      tl;dr just answer the fuckin questions.
     </div>
     <div id="questions">
       <?php include "generate.php" ?>
